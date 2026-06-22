ae968c0475ef82a94096b4f36bd0a99ff268522d
copyfreetv <copytv.iptv@gmail.com>
2026-06-22 11:18:17 +0700

    Update hls.js

--- api/hls.js
+++ api/hls.js
@@ -5,21 +5,51 @@ export default async function handler(req, res) {
   const target =
     "http://45.139.122.199:2095" + path;
 
-  const response = await fetch(target);
+  const headers = {};
+
+  if (req.headers.range) {
+    headers.Range = req.headers.range;
+  }
+
+  const response = await fetch(target, {
+    headers
+  });
 
   const buffer = Buffer.from(
     await response.arrayBuffer()
   );
 
-  res.setHeader(
-    "Content-Type",
-    "video/mp2t"
-  );
+  res.status(response.status);
 
   res.setHeader(
     "Access-Control-Allow-Origin",
     "*"
   );
 
-  res.status(200).send(buffer);
+  res.setHeader(
+    "Accept-Ranges",
+    "bytes"
+  );
+
+  const contentType =
+    response.headers.get("content-type");
+
+  if (contentType) {
+    res.setHeader(
+      "Content-Type",
+      contentType
+    );
+  }
+
+  const contentRange =
+    response.headers.get("content-range");
+
+  if (contentRange) {
+    res.setHeader(
+      "Content-Range",
+      contentRange
+    );
+  }
+
+  res.send(buffer);
 }
